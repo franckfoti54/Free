54c06ddcf58f0c00357abed00bd56eecae8c4fe1
franckfoti54 <franckfoti54@gmail.com>
2025-03-09 00:49:04 +0100

    Update aihd.py

--- aihd.py
+++ aihd.py
@@ -1,3 +1,25 @@
-3                         ("system", "You are a geog    raphy expert that returns the colors present in a     country's flag en francais."),                     14                                 ("human", "France"    ),                                                 15                                         ("ai", "bl    ue, white, red"),                                  16                                                 ("    human", "{country}")                               17                                                         ]                                                18             )                                      19                                                    20 # Chain the prompt template and model, and invoke
-    the chain                                          21 llm_chain = prompt_template | llm                  22                                                    23 country = "Cameroon"
- 24 response = llm_chain.invoke({"country": country})  25 print("les couleurs que l'on tetrouve sur le drapa    u de ce pays sont: "+response.content)
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAI
+
+from langchain_core.prompts import ChatPromptTemplate
+
+
+
+llm = ChatOpenAI(model="gpt-4o-mini", api_key='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')   
+
+# Create a chat prompt template
+prompt_template = ChatPromptTemplate.from_messages(
+            [
+                        ("system", "You are a geography expert that returns the colors present in a country's flag en francais."),
+                                ("human", "France"),
+                                        ("ai", "blue, white, red"),
+                                                ("human", "{country}")
+                                                    ]
+            )
+
+# Chain the prompt template and model, and invoke the chain
+llm_chain = prompt_template | llm
+
+country = "Cameroon"
+response = llm_chain.invoke({"country": country})
+print("les couleurs que l'on tetrouve sur le drapau de ce pays sont: "+response.content)
